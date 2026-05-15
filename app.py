b59027e991afe1a2000a9142f375c9b2b224424b
oglmalasia-stack <oglmalasia@gmail.com>
2026-05-15 14:58:13 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+# Configura a página para usar a tela inteira
+st.set_page_config(layout="wide")
+st.markdown("# :blue[Nordcraft]")
+st.title("Kits")
+st.markdown("### :red[Netherite] :grey[5,00]")
+st.markdown("### :blue[Diamante] :grey[4,00]")
+st.markdown("### :green[Farms] :grey[3,50]")
+st.markdown("### :violet[Cristal] :grey[4,50]")
+st.markdown("### :orange[Tnt] :grey[4,50]")
+st.markdown("### :red[Redstone] :grey[3,50]")
